8b4e0716999ffb8dd8aa8ebec0056c0f9b55682d
rugbratsss <rugbratsss@gmail.com>
2026-04-24 21:56:53 +0200

    Refactor populateRatings method for improved input handling and formatting

--- StreamingServicesApplication.java
+++ StreamingServicesApplication.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
 import java.util.Random;
 public class StreamingServicesApplication {
-    public static int populateRatings(int[] userRatings, String[] serviceProviders)
+    public static void populateRatings(int[] userRatings, String[] serviceProviders)
     {
         Scanner kb = new Scanner(System.in);
         for(int i = 0; i < userRatings.length; i++)
         {
-            System.out.println("Enter user ratings(1-5) for " + serviceProviders[i]);
-            userRatings[i] = kb.nextInt();
+                System.out.print("Enter user ratings(1-5) for " + serviceProviders[i] + ": ");
+                userRatings[i] = kb.nextInt();
+                while(userRatings[i] > 1 || userRatings[i] < 5)
+                {
+                    System.out.print("Invalid input, enter a number from 1 to 5: ");
+                    userRatings[i] = kb.nextInt();
+                }
+              
         }
-    }
     }
     public static void populateSubscribers(int[] subscribers)
     {
         Random rand = new Random();
 
-        for(int i = 0; i < subscribers.length; i++){
+        for(int i = 0; i < subscribers.length; i++)
+        {
             subscribers[i] = rand.nextInt(400001) + 100000;
         }
     }
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         String[] serviceProviders = {"YouTube", "Netflix","DSTV", "ShowMax", "PrimeTV", "SABC"};
         String[] originCountry = {"United States", "United States", "South Africa", "South Africa", "United States", "South Africa"}; 
 
         int[] subscribers = new int [serviceProviders.length];
         int[] userRatings = new int [serviceProviders.length];
+
         populateSubscribers(subscribers);
         populateRatings(userRatings,serviceProviders);
     }
